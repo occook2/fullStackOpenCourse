16ec47b7cfb0140a301b7c09798959acd3675f27
occook2 <119069061+occook2@users.noreply.github.com>
2024-06-29 13:56:12 -0400

    Rough Draft of 1.1, 1.2

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,61 @@
+const Header = (props) => {
+  return (
+    <>
+      <h1>{props.course}</h1>
+    </>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <>
+      <p>{props.part.part} {props.part.exercise}</p>
+    </>
+  )
+}
+const Content = (props) => {
+  return (
+    <>
+      <Part part={props.parts[0]}/>
+      <Part part={props.parts[1]}/>
+      <Part part={props.parts[2]}/>
+    </>
+  )
+}
+
+const Total = (props) => {
+  console.log(props.parts)
+  var total = 0
+  for (var i=0; i < props.parts.length; i++) {
+    total = total + props.parts[i].exercise
+  }
+
+  return (
+    <>
+      <p>Number of exercises {total}</p>
+    </>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+  const parts = [{part: part1, exercise: exercises1}, 
+    {part: part2, exercise: exercises2}, 
+    {part: part3, exercise: exercises3}]
+
+  return (
+    <div>
+      <Header course={course}/>
+      <Content parts={parts} />
+      <Total parts={parts} />
+    </div>
+  )
+}
+
+export default App
