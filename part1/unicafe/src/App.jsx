e7b3dbf33e500347e6d2a270d80793206cfbcbf5
occook2 <119069061+occook2@users.noreply.github.com>
2024-07-01 20:55:24 -0400

    Finished rough draft of unicafe

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,34 +1,90 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const Header = () => {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>How was your experience with unicafe?</h1>
     </>
+  )
+}
+
+const Button = ({ handleClick, text }) => {
+  return (
+    <button onClick = {handleClick}>
+      {text}
+    </button>
+  )
+}
+
+const StatisticLine = ({ text, value }) => {
+  return (
+    <>
+      <tr>
+        <th>{text}</th>
+        <th>{value}</th>
+      </tr> 
+    </>
+  )
+}
+
+const Statistics = ({ good, neutral, bad }) => {
+  const findTotal = () => good + neutral + bad
+  const findAverage = () => {
+    if (findTotal() === 0) return 0
+    else return (good - bad)/findTotal()
+  }
+  const findPercentPositive = () => {
+    if (findTotal() === 0) return 0
+    else return good/findTotal()*100
+  }
+  
+  if (findTotal() === 0) {
+    return (
+      <div>
+        <h2>Statistics</h2>
+        <p>No feedback has been given</p>
+      </div>
+    )
+  }
+  else 
+  {
+    return (
+      <>
+        <h2>Statistics</h2>
+        <table>
+          <tbody>
+            <StatisticLine text='Good' value={good} />
+            <StatisticLine text='Neutral' value={neutral} />
+            <StatisticLine text='Bad' value={bad} />
+            <StatisticLine text='Total' value={findTotal()} />
+            <StatisticLine text='Average' value={findAverage()} />
+            <StatisticLine text='Percent Positive Reviews' value={findPercentPositive()} />
+          </tbody>
+        </table>
+      </>
+    )
+  }
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGood = () => setGood(good+1)
+  const handleNeutral = () => setNeutral(neutral+1)
+  const handleBad = () => setBad(bad+1)
+
+  return (
+    <div>
+      <Header />
+      <Button handleClick={handleGood} text={'Good'} />
+      <Button handleClick={handleNeutral} text={'Neutral'} />
+      <Button handleClick={handleBad} text={'Bad'} />
+      <Statistics good={good} neutral={neutral} bad={bad}/>
+    </div>
   )
 }
 
