3ba25673fafa77ab1befbb37e23b08a6bf617c48
occook2 <119069061+occook2@users.noreply.github.com>
2024-07-08 16:55:31 -0400

    Started Countries Project

---
+++ part2/countries/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      Hello Countries Project
+    </>
+  )
+}
+
+export default App
