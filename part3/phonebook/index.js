ac6c32da4e258be9f4020c3db417b59930dccf6a
occook2 <119069061+occook2@users.noreply.github.com>
2024-10-28 09:22:09 -0400

    Completed Exercises for 3a

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,10 +1,11 @@
 const express = require('express')
-const app = express()
-
 const morgan = require('morgan')
 
+const app = express()
 app.use(express.json())
-app.use(morgan('tiny'))
+
+morgan.token('body', function getBody (req, res) { return JSON.stringify(req.body) })
+app.use(morgan(':method :url :status :res[content-length] :response-time ms :body'))
 
 let persons = [
     { 
@@ -29,13 +30,17 @@ let persons = [
     }
 ]
 
+app.get('/info', (request,response) => {
+    response.send(`<p>Phonebook has info for ${persons.length} people<p><p>${Date()}<p>`)
+})
+
 app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
 app.get('/api/persons/:id', (request, response) => {
     const id = request.params.id
-    const person = persons.find(p => p.id === id)
+    const person = persons.find(person => person.id === id)
     if (person) response.json(person)
     else response.status(404).end()
 })
@@ -44,39 +49,38 @@ app.delete('/api/persons/:id', (request, response) => {
     const id = request.params.id
     persons = persons.filter(person => person.id !== id)
     response.status(204).end()
-})
+}) 
+
+const generateId = () => {
+    return String(Math.floor(Math.random()*10000000))
+}
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
 
-    // Input Validation
-    if (!body.number || !body.name) {
+    if (!body.name || !body.number) {
         return response.status(400).json({
-            error: "Content Missing"
+            error: 'Name or number are missing'
         })
     }
-    if (persons.find(person => person.name === body.name)) {
+    else if (persons.find(person => person.name === body.name)) {
         return response.status(400).json({
-            error: "Name Taken"
+            error: 'Name already exists in the phonebook'
         })
     }
 
-    const newPerson = {
-        "name": body.name,
-        "number": body.number,
-        "id": String(Math.floor(Math.random()*1000000)) 
+    const person = {
+        id: generateId(),
+        name: body.name,
+        number: body.number,
     }
-    persons = persons.concat(newPerson)
 
-    response.json(newPerson)    
-})
+    persons = persons.concat(person)
 
-app.get('/info', (request, response) => {
-    response.send(`<p>The phonebbok has ${persons.length} people</p><p>${new Date(Date.now())}</p>`)
+    response.json(person)
 })
 
 const PORT = 3001
 app.listen(PORT, () => {
-    console.log(`Server running on ${PORT}`)
+    console.log(`Server is running on port ${PORT}`)
 })
-
